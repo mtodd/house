e62f1d653415c1beeea4e2f8610f2dda77779eb5
Matt Todd <chiology@gmail.com>
2008-11-30 05:17:58 -0500

    Added migration for products.

---
+++ schema/migrations/001_create_products_migration.rb
@@ -0,0 +1,16 @@
+migration 1, :create_products  do
+  up do
+    create_table :products do
+      column :id,         Integer, :serial => true
+      column :name,       String
+      column :sku,        String
+      column :aisle,      String
+      column :bin,        String
+      column :created_at, DateTime
+      column :updated_at, DateTime
+    end
+  end
+  down do
+    drop_table :products
+  end
+end
